bc738828c2e53f1b41f95846ebd6507ec7547d90
OIIUOI <eunjin4788@gmail.com>
2023-04-11 23:24:19 +0900

    make question

--- ifdog/urls.py
+++ ifdog/urls.py
@@ -11,12 +11,29 @@ app_name = 'ifdog'
 urlpatterns = [
     path('q1/', views.Q1_Social.as_view(), name = "q1-social"),
     path('q2_act/', views.Q2_Activity.as_view(), name = "q2-act"),
-    path('test/', views.test, name='test'),
-    # path('q2_act/', views.Q3_Aggressive.as_view(), name = "q3-agg"),
-    # path('q2_act/', views.Q3_Stamina.as_view(), name = "q3-sta"),
-    # path('q2_act/', views.Q2_Activity.as_view(), name = "q2-act"),
-    # path('q2_act/', views.Q2_Activity.as_view(), name = "q2-act"),
-    # path('q2_act/', views.Q2_Activity.as_view(), name = "q2-act"),
-    # path('q2_act/', views.Q2_Activity.as_view(), name = "q2-act"),
-    # path('q2_act/', views.Q2_Activity.as_view(), name = "q2-act"),
+    path('q3_agg/', views.Q3_Aggressive.as_view(), name = "q3-agg"),
+    path('q3_stam/', views.Q3_Stamina.as_view(), name = "q3-stam"),
+    path('q4_act/', views.Q4_Independence.as_view(), name = "q4-ind"),
+    path('q4_act/', views.Q4_Learn.as_view(), name = "q4-learn"),
+
+    path('q2_stam/', views.Q2_Stamina.as_view(), name = "q2-stam"),
+    path('q3_ag/', views.Q3_Agg.as_view(), name = "q3-ag"),
+    path('q3_ind/', views.Q3_Agg.as_view(), name = "q3-ind"),
+    
+    
+    ################## result ####################
+    path('r-grayhound/', views.R_Grayhound.as_view(), name = "r-gray"),
+    path('r-sivaInu/', views.R_SivaInu.as_view(), name = "r-siva"),
+    path('r-jindo/', views.R_Jindo.as_view(), name = "r-jindo"),
+    
 ]
+
+''' 
+r-shihtzu
+r-chihuahua
+r-husky
+r-corgi
+r-pomeranian
+r-yorkshire
+r-Bedlington_Terrier
+'''
--- ifdog/views.py
+++ ifdog/views.py
@@ -6,80 +6,151 @@ from django.http import JsonResponse
 # Create your views here.
 
 class Q1_Social(TemplateView):
-    template_name = "question/q1.html"
+    template_name = "question/social.html"
 
     def post(self, request):
-        q1, q2, q3 = request.POST.get("q1")
-        print(q1)
+        [q1, q2, q3] = map(int, [request.POST.get("q1"), request.POST.get("q2"), request.POST.get("q3")])
+        score = q1 + q2 + q3
         
-        if q1 == "-2":
+        if score < -1:
             return redirect('ifdog:q2-act')
-        # elif score == "mid":
-        #     return redirect('ifdog:q2-strength')
-        # else:
-        #     return redirect('ifdog:q2-activity')
-        return redirect('ifdog:q2-at')
-
-def test(request):
-    if request.method == "POST":
-        print(request.POST)
-        return redirect('ifdog:q2-act')
-    return render(request, 'question/q1.html')
+        elif score < 1:
+            return redirect('ifdog:q2-strength')
+        else:
+            return redirect('ifdog:q2-activity')
         
 class Q2_Activity(TemplateView):
-    template_name = "question/q2.html"
+    template_name = "question/act.html"
 
-    # def post(self, request):
-    #     score = request.POST.get("score")
-    #     if score == "calm":
-    #         return redirect('ifdog:q3-agg')
-    #     else:
-    #         return redirect('ifdog:q3-sta')
+    def post(self, request):
+        [q1, q2, q3] = map(int,[
+            request.POST.get("q1"), request.POST.get("q2"), request.POST.get("q3")
+            ])
+        score = q1 + q2 + q3
         
-# class Q3_Aggressive(View):
-#     def get(self, request):
-#         return render(request, 'ifdog:q3-agg')
-
-#     def post(self, request):
-#         score = request.POST.get("score")
-#         if score == "up":
-#             return redirect('r-jindo')
-#         elif score == "middle":
-#             return redirect('r-siba')
-#         else:
-#             return redirect('r-gray')
+        if score <= 0:
+            return redirect('ifdog:q3-agg')
+        else:
+            return redirect('ifdog:q3-stam')
         
-# class Q3_Stamina(View):
-#     def get(self, request):
-#         return render(request, 'ifdog:q3-sta')
+class Q3_Aggressive(TemplateView):
+    template_name = "question/agg.html"
 
-#     def post(self, request):
-#         score = request.POST.get("score")
-#         if score == "up":
-#             return redirect('ifdog:q4-learn')
-#         elif score == "middle":
-#             return redirect('r-bedlington')
-#         else:
-#             return redirect('ifdog:q4-ind')
-
-# class Q4_Independence(View):
-#     def get(self, request):
-#         return render(request, 'ifdog:q4-ind')
-
-#     def post(self, request):
-#         score = request.POST.get("score")
-#         if score == "independent":
-#             return redirect('r-yorkshire')
-#         else:
-#             return redirect('r-pomeranian')
+    def post(self, request):
+        [q1, q2, q3] = map(int, [
+            request.POST.get("q1"), request.POST.get("q2"), request.POST.get("q3")
+            ])
+        score = q1 + q2 + q3
         
-# class Q4_Learn(View):
-#     def get(self, request):
-#         return render(request, 'ifdog:q4-learn')
+        if score < -1:
+            return redirect('ifdog:r-grayhound')
+        elif score < 1:
+            return redirect('ifdog:r-siva')
+        else:
+            return redirect('ifdog:r-zindo')
+        
+class Q3_Stamina(TemplateView):
+    template_name = "question/stem.html"
 
-#     def post(self, request):
-#         score = request.POST.get("score")
-#         if score == "high":
-#             return redirect('r-corgi')
-#         else:
-#             return redirect('r-husky')
+    def post(self, request):
+        [q1, q2, q3] = map(int, [
+            request.POST.get("q1"), request.POST.get("q2"), request.POST.get("q3")
+            ])
+        score = q1 + q2 + q3
+        
+        if score < -1:
+            return redirect('ifdog:ind')
+        elif score < 1:
+            return redirect('ifdog:r-bed')
+        else:
+            return redirect('ifdog:q4-learn')
+
+class Q4_Independence(TemplateView):
+    template_name = "question/ind.html"
+
+    def post(self, request):
+        [q1, q2, q3] = map(int, [
+            request.POST.get("q1"), request.POST.get("q2"), request.POST.get("q3")
+            ])
+        score = q1 + q2 + q3
+        if score <= 0:
+            return redirect('r-yorkshire')
+        else:
+            return redirect('r-pomeranian')
+            
+            
+        
+class Q4_Learn(TemplateView):
+    template_name = "question/learn.html"
+
+    def post(self, request):
+        [q1, q2, q3] = map(int, [
+            request.POST.get("q1"), request.POST.get("q2"), request.POST.get("q3")
+            ])
+        score = q1 + q2 + q3
+
+        if score <= 0:
+            return redirect('r-husky')
+        else:
+            return redirect('r-corgi')
+            
+class Q2_Stamina(TemplateView):
+    template_name = "question/stem.html"
+
+    def post(self, request):
+        [q1, q2, q3] = map(int, [
+            request.POST.get("q1"), request.POST.get("q2"), request.POST.get("q3")
+            ])
+        score = q1 + q2 + q3
+        
+        if score < -1:
+            return redirect('ifdog:q3-agg')
+        elif score < 1:
+            return redirect('ifdog:q3-ind')
+        else:
+            return redirect('ifdog:q3-ins')
+
+class Q3_Agg(TemplateView):
+    template_name = "question/agg.html"
+
+    def post(self, request):
+        [q1, q2, q3] = map(int, [
+            request.POST.get("q1"), request.POST.get("q2"), request.POST.get("q3")
+            ])
+        score = q1 + q2 + q3
+        
+        if score < 1:
+            return redirect('ifdog:r-shihtzu')
+        else:
+            return redirect('ifdog:r-chihuahua')
+
+class Q3_Independence(TemplateView):
+    template_name = "question/ind.html"
+
+    def post(self, request):
+        [q1, q2, q3] = map(int, [
+            request.POST.get("q1"), request.POST.get("q2"), request.POST.get("q3")
+            ])
+        score = q1 + q2 + q3
+        if score <= 0:
+            return redirect('r-yorkshire')
+        else:
+            return redirect('r-pomeranian')
+        
+
+
+        
+################## RESULT #####################
+
+class R_Grayhound(TemplateView):
+    template_name = "result/r-grayhound.html"
+
+
+class R_SivaInu(TemplateView):
+    template_name = "result/r-sivainu.html"
+
+class R_Jindo(TemplateView):
+    template_name = "result/r-jindo.html"
+
+class R_Bedlington(TemplateView):
+    template_name = "result/r-bed.html"
