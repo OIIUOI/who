e6f337f0ce6c176ea36faa6cd161d74db7a809da
OIIUOI <eunjin4788@gmail.com>
2023-08-16 01:11:46 +0900

    wsgi modify

--- who/wsgi.py
+++ who/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'who.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'who.settings')
 
 application = get_wsgi_application()
